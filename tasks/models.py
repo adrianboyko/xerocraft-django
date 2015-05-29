ef264e89cf9977b9c86f3de1492a33401281a6a5
adrianboyko <AdrianBoyko@gmail.com>
2015-05-29 11:10:07 -0700

    Added appropriate on_deletes to ForeignKeys. Made blank=True, where appropriate.

--- tasks/models.py
+++ tasks/models.py
@@ -11,7 +11,7 @@ class Member(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
     user_id = models.CharField(max_length=40, help_text="The user-id the member uses to sign in at Xerocraft.")
-    family = models.ForeignKey('self', null=True, help_text="If this member is part of a family account then this points to the 'anchor' member for the family.")
+    family = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, help_text="If this member is part of a family account then this points to the 'anchor' member for the family.")
     tags = models.ManyToManyField(Tag)
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
@@ -22,7 +22,7 @@ class RecurringTaskTemplate(models.Model):
     "Uses a day-of-week vs nth-of-month matrix to define a schedule for recurring tasks"
     # Copied to instances:
     short_desc = models.CharField(max_length=TASK_SHORT_DESC_LEN, help_text="A description that will be copied to instances of the recurring task.")
-    reviewer = models.ForeignKey(Member, null=True, help_text="A reviewer that will be copied to instances of the recurring task.")
+    reviewer = models.ForeignKey(Member, null=True, blank=True, on_delete=models.SET_NULL, help_text="A reviewer that will be copied to instances of the recurring task.")
     work_estimate = models.IntegerField(default=0, help_text="Provide an estimate of how much work this tasks requires, in minutes. This is work time, not elapsed time.")
     # Specific to template
     start_date = models.DateField(help_text="Choose a date for the first instance of the recurring task.")
@@ -57,16 +57,16 @@ class Task(models.Model):
     # For recurring tasks, these will be copied from RecurringTaskTemplate:
     short_desc = models.CharField(max_length=TASK_SHORT_DESC_LEN, help_text="A very short description of the task which will be used as it's name. Don't try to provide detailed instructions here - attach a TaskNote instead.")
     work_estimate = models.IntegerField(default=0, help_text="Provide an estimate of how much work this tasks requires, in minutes. This is work time, not elapsed time.")
-    reviewer = models.ForeignKey(Member, null=True, related_name="tasks_to_review", help_text="If required, a member who will review the completed work and either accept or reject it.")
+    reviewer = models.ForeignKey(Member, null=True, blank=True, on_delete=models.SET_NULL, related_name="tasks_to_review", help_text="If required, a member who will review the completed work and either accept or reject it.")
     # Other fields:
-    deadline = models.DateField(null=True, help_text="If appropriate, specify a deadline by which the task must be completed.")
+    deadline = models.DateField(null=True, blank=True, help_text="If appropriate, specify a deadline by which the task must be completed.")
     depends_on = models.ManyToManyField('self', related_name="prerequisite_for", help_text="If appropriate, specify what tasks must be completed before this one can start.")
-    claim_date = models.DateField(null=True)
-    claimed_by = models.OneToOneField(Member, null=True, related_name="tasks_claimed")
-    prev_claimed_by =  models.ForeignKey(Member, null=True, related_name="+") # Reminder: "+" means no backwards relation.
+    claim_date = models.DateField(null=True, blank=True)
+    claimed_by = models.OneToOneField(Member, null=True, blank=True, related_name="tasks_claimed")
+    prev_claimed_by =  models.ForeignKey(Member, null=True, blank=True, on_delete=models.SET_NULL, related_name="+") # Reminder: "+" means no backwards relation.
     work_done = models.BooleanField(default=False)
     work_accepted = models.NullBooleanField()
-    recurring_task_template = models.ForeignKey(RecurringTaskTemplate, null=True)
+    recurring_task_template = models.ForeignKey(RecurringTaskTemplate, null=True, blank=True, on_delete=models.SET_NULL)
     def is_closed(self):
         "Returns True if claimant should receive credit for the task."
         if self.reviewer == None:
@@ -87,6 +87,6 @@ class Task(models.Model):
             return False, "Invalid work estimate."
 
 class TaskNote(models.Model):
-    author = models.ForeignKey(Member)
+    author = models.ForeignKey(Member, null=True, blank=True, on_delete=models.SET_NULL) # Note will become anonymous if member is deleted.
     content = models.TextField(2048, help_text="Anything you want to say about the task. Instructions, hints, requirements, review feedback, etc.")
-    task = models.ForeignKey(Task)
+    task = models.ForeignKey(Task) # default on_delete, i.e. delete note if task is deleted.
