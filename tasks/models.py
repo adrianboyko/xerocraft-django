fc638b58200ad9fe3c30a322906c5e2799d63599
adrianboyko <AdrianBoyko@gmail.com>
2015-06-02 11:12:07 -0700

    Model (and associated test case) work continues.

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone
+from datetime import date, timedelta
+
+
+# TODO: Rework various validate() methods into Model.clean()? See Django's "model validation" docs.
+# TODO: Use local dates, not datetimes.
 
 class Tag(models.Model):
     name = models.CharField(max_length=40, help_text="A short name for the tag.")
@@ -9,7 +14,7 @@ class Tag(models.Model):
         return self.name
 
 class Member(models.Model):
-    "Represents a Xerocraft member, in their many varieties."
+    """Represents a Xerocraft member, in their many varieties."""
 
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
@@ -28,10 +33,14 @@ class Member(models.Model):
         return "%s %s" % (self.first_name, self.last_name)
 
 def make_task_mixin(related_name_val):
+    """This function tunes the mix-in by allowing the related_name for eligible_claimants and eligible_tags to be specified.
+    The rest of the mix-in is identical for both Task and RecurringTaskTemplate.
+    """
 
     class TaskMixin(models.Model):
         """Defines fields that are common between RecurringTaskTemplate and Task.
-        When a task is created from the template, these fields are copied from the template to the task."""
+        When a task is created from the template, these fields are copied from the template to the task.
+        """
 
         short_desc = models.CharField(max_length=40, help_text="A description that will be copied to instances of the recurring task.")
         eligible_claimants = models.ManyToManyField(Member, symmetrical=False, related_name=related_name_val, help_text="Anybody listed is eligible to claim the task")
@@ -44,18 +53,18 @@ def make_task_mixin(related_name_val):
     return TaskMixin
 
 class RecurringTaskTemplate(make_task_mixin("+")):
-    "Uses a 'day-of-week vs nth-of-month' matrix to define a schedule for recurring tasks."
+    """Uses a 'day-of-week vs nth-of-month' matrix to define a schedule for recurring tasks."""
 
     start_date = models.DateField(help_text="Choose a date for the first instance of the recurring task.")
     suspended = models.BooleanField(default=False, help_text="Additional tasks will not be created from this template while it is suspended.")
 
-    # Week of month:
-    first_week = models.BooleanField(default=False, help_text="Task will recur on first weekday in the month.")
-    second_week = models.BooleanField(default=False, help_text="Task will recur on second weekday in the month.")
-    third_week = models.BooleanField(default=False, help_text="Task will recur on third weekday in the month.")
-    fourth_week = models.BooleanField(default=False, help_text="Task will recur on fourth weekday in the month.")
-    last_week = models.BooleanField(default=False, help_text="Task will recur on last weekday in the month. This will be 4th or 5th weekday, depending on calendar.")
-    every_week = models.BooleanField(default=False, help_text="Task recur every week")
+    # Weekday of month:
+    first = models.BooleanField(default=False, help_text="Task will recur on first weekday in the month.")
+    second = models.BooleanField(default=False, help_text="Task will recur on second weekday in the month.")
+    third = models.BooleanField(default=False, help_text="Task will recur on third weekday in the month.")
+    fourth = models.BooleanField(default=False, help_text="Task will recur on fourth weekday in the month.")
+    last = models.BooleanField(default=False, help_text="Task will recur on last weekday in the month. This will be 4th or 5th weekday, depending on calendar.")
+    every = models.BooleanField(default=False, help_text="Task recur every week")
 
     # Day of week:
     monday = models.BooleanField(default=False, help_text="Task will recur on Monday.")
@@ -66,13 +75,72 @@ class RecurringTaskTemplate(make_task_mixin("+")):
     saturday = models.BooleanField(default=False, help_text="Task will recur a Saturday.")
     sunday = models.BooleanField(default=False, help_text="Task will recur a Sunday.")
 
-    def last_instance():
-        "Looks at the Tasks that correspond to this template and returns the the one with the greatest scheduled_date."
+    def greatest_scheduled_date(self):
+        "Of the Tasks that correspond to this template, returns the greatest scheduled_date."
 
-    def validate(self): # TODO: Rework this into Model.clean(). See Django's "model validation" docs.
-        if self.last_week and self.fourth_week:
+        if len(self.task_set.all()) == 0:
+            # Nothing is scheduled yet but nothing can be scheduled before start_date.
+            # So, pretend that day before start_date is the greatest scheduled date.
+            result = self.start_date + timedelta(days = -1)
+            return result
+
+        scheduled_dates = map(lambda x: x.scheduled_date, self.task_set)
+        return max(scheduled_dates)
+
+    def date_matches_template(self, d: date):
+        """
+        :param d: Date to be tested.
+        :return: Boolean indicating if d matches the day-of-week and ordinal-in-month specified by the template.
+        """
+        dow_num = d.weekday() # day-of-week number
+        day_matches = (dow_num==0 and self.monday) \
+            or (dow_num==1 and self.tuesday) \
+            or (dow_num==2 and self.wednesday) \
+            or (dow_num==3 and self.thursday) \
+            or (dow_num==4 and self.friday) \
+            or (dow_num==5 and self.saturday) \
+            or (dow_num==6 and self.sunday)
+
+        if not day_matches: return False  # Doesn't match template if day-of-week doesn't match.
+        if self.every: return True  # Does match if it happens every week and the day-of-week matches.
+
+        # Otherwise, figure out the ordinal and see if we match it.
+        dom_num = d.day
+        ord_num = 1
+        while dom_num > 7:
+            dom_num -= 7
+            ord_num += 1
+        ordinal_matches = (ord_num==1 and self.first==True) \
+            or (ord_num==2 and self.second==True) \
+            or (ord_num==3 and self.third==True) \
+            or (ord_num==4 and self.fourth==True) \
+            or (ord_num==4 and self.last==True) \
+            or (ord_num==5 and self.last==True)
+
+        return ordinal_matches
+
+    def create_tasks(self, max_days_in_advance):
+        """Creates and schedules new tasks from greatest_scheduled_date() (non-inclusive).
+        Stops when scheduling a new task would be more than max_days_in_advance from current date.
+        """
+
+        curr = self.greatest_scheduled_date() + timedelta(days = +1)
+        stop = date.today() + timedelta(max_days_in_advance)
+        while curr < stop:
+            if self.date_matches_template(curr):
+                t = Task.objects.create(recurring_task_template = self)
+                t.short_desc = self.short_desc
+                t.eligible_claimants = self.eligible_claimants.all()
+                t.eligible_tags = self.eligible_tags.all()
+                t.reviewer = self.reviewer
+                t.work_estimate = self.work_estimate
+            curr += timedelta(days = +1)
+
+    def validate(self):
+        sd = self.start_date
+        if self.last and self.fourth:
             return False, "Choose either fourth week or last week, not both."
-        if self.every_week and (self.first_week or self.second_week or self.third_week or self.fourth_week or self.last_week):
+        if self.every and (self.first or self.second or self.third or self.fourth or self.last):
             return False, "If you choose 'every week' don't choose any other weeks."
         if self.work_estimate < 0:
             # zero will mean "not yet estimated" but anything that has been estimated must have work > 0.
@@ -102,16 +170,16 @@ class Task(make_task_mixin("claimable_tasks")):
         "Returns True if the task isn't yet completed or if there's a reviewer who hasn't yet accepted it."
         return not self.is_closed()
 
-    def validate(self): # TODO: Rework this into Model.clean(). See Django's "model validation" docs.
+    def validate(self):
         # TODO: questionable if deadline is set but task is an instance of RecurringTaskTemplate.
-        if work_accepted and not work_done:
+        if self.work_accepted and not self.work_done:
             return False, "Work cannot be reviewed before it is marked as completed."
-        if prev_claimed_by == claimed_by:
+        if self.prev_claimed_by == self.claimed_by:
             return False, "Member cannot claim a task they've previously claimed. Somebody else has to get a chance at it."
         if self.work_estimate < 0:
             # zero will mean "not yet estimated" but anything that has been estimated must have work > 0.
             return False, "Invalid work estimate."
-        if self.recurring_task_template is not None and scheduled_date is None:
+        if self.recurring_task_template is not None and self.scheduled_date is None:
             return False, "A task corresponding to a ScheduledTaskTemplate must have a scheduled date."
         return True, "Looks good."
 
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
-from tasks.models import Member, RecurringTaskTemplate
+from tasks.models import Member, RecurringTaskTemplate, Task
+from datetime import date, timedelta
 
-class TestMember(TestCase):
+class TestMemberValidity(TestCase):
+
+    def test_member_rules_against_db(self):
+        for m in Member.objects.all():
+            valid,_ = m.validate()
+            self.assertTrue(valid)
+
+class TestMemberWithFamily(TestCase):
 
     def setUp(self):
         ab = Member.objects.create(first_name="Andrew", last_name="Baker", user_id="fake1")
@@ -20,13 +28,28 @@ class TestMember(TestCase):
         self.assertEqual(jr.family_anchor, ab)
         self.assertTrue(jr in ab.family_members.all())
 
-    def test_member_rules_against_db(self):
-        for m in Member.objects.all():
-            valid,_ = m.validate()
+class TestRecurringTaskTemplateValidity(TestCase):
+    def test_RecurringTaskTemplate_rules_against_db(self):
+        for rtt in RecurringTaskTemplate.objects.all():
+            valid,_ = rtt.validate()
             self.assertTrue(valid)
 
-class TestRecurringTask(TestCase):
+class TestRecurringTaskTemplate(TestCase):
 
     def setUp(self):
-        rt1 = RecurringTaskTemplate.objects.create(short_desc="A test",work_estimate=1.5)
+        self.rt = RecurringTaskTemplate.objects.create(
+            short_desc = "A test",
+            work_estimate = 1.5,
+            start_date = date.today(),
+            last = True,
+            sunday = True)
 
+    def test_greatest_scheduled_date(self):
+        sched = self.rt.greatest_scheduled_date()
+        start = self.rt.start_date
+        self.assertTrue(type(sched) is date and type(start) is date)
+        self.assertGreater(self.rt.start_date, sched)
+
+    def test_create_tasks(self):
+        self.rt.create_tasks(max_days_in_advance=28)
+        self.assertEqual(len(Task.objects.all()),1)
