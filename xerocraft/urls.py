4880588e55c08f7b6bfaef36c037e209f9e733d3
adrianboyko <AdrianBoyko@gmail.com>
2015-06-08 13:16:22 -0700

    Added url files for tasks and inventory apps.

--- xerocraft/urls.py
+++ xerocraft/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^tasks/', include('tasks.urls')),
+    url(r'^inventory/', include('inventory.urls')),
 ]
