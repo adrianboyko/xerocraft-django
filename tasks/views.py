068fa586718a86b1269c961ec21abc010ce6a047
adrianboyko <AdrianBoyko@gmail.com>
2015-06-08 13:23:38 -0700

    Starting views and templates.

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from tasks.models import Task
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('tasks/index.html')
+    return HttpResponse(template.render(None))
+
+def list(request):
+    """ This view will present all tasks with status and optional filtering.
+    """
+    task_list = Task.objects.all()
+    template = loader.get_template('tasks/list.html')
+    context = RequestContext(request, {'task_list': task_list})
+    return HttpResponse(template.render(context))
