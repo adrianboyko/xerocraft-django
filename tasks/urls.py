fe4b353d536b67c349f5e890ffb18c3949069db0
adrian <AdrianBoyko@gmail.com>
2015-09-17 14:39:38 -0700

    Views to handle email nag clicks now ask about adjacent open/close tasks.

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^offer-task/(?P<task_pk>[0-9]+)_(?P<auth_token>[-_a-zA-Z0-9]{32})/$', views.offer_task, name='offer-task'),
     url(r'^offer-more-tasks/(?P<task_pk>[0-9]+)_(?P<auth_token>[-_a-zA-Z0-9]{32})/$', views.offer_more_tasks, name='offer-more-tasks'),
     url(r'^offer-adjacent-tasks/(?P<auth_token>[-_a-zA-Z0-9]{32})/$', views.offer_adjacent_tasks, name='offer-adjacent-tasks'),
+    url(r'^offers-done/(?P<auth_token>[-_a-zA-Z0-9]{32})/$', views.offers_done, name='offers-done'),
 
 
     # Calendars
