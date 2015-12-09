e907c8691be69e07338654de85ebebebf29643f9
adrian <AdrianBoyko@gmail.com>
2015-12-08 22:57:08 -0700

    Starting to set up "/" as the entry point for people using ANY computer at Xerocraft.  This is different than using the kiosk since the kiosk will have a card reader.

--- xerocraft/views.py
+++ xerocraft/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import render
+from django.contrib import auth
 __author__ = 'Adrian'
 
-from django.shortcuts import render
 
 def index(request):
+    auth.logout(request)  # Helps protect against people who forget to log out.
     return render(request, 'xerocraft/xerocraft-home.html',{})
