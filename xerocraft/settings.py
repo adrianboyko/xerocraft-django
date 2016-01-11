d1bbe35ddb9560a297bd825fcfc1d0bbc664b5dd
adrian <AdrianBoyko@gmail.com>
2016-01-11 14:09:35 -0700

    Set DEBUG to FALSE but might change this to an environment variable in the future.

--- xerocraft/settings.py
+++ xerocraft/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['XERO_DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False  # IMPORTANT: Set to False before commit.
 
 ALLOWED_HOSTS = []
 
