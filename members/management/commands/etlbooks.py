e8a98c213b4ed1fb28a08a99b4bf07bab7a0bac0
adrian <AdrianBoyko@gmail.com>
2016-01-18 15:20:21 -0700

    Added WePay fetcher for paid membership ETL.

--- members/management/commands/etlbooks.py
+++ members/management/commands/etlbooks.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from members.models import PaidMembership
-from . fetchers import TwoCheckoutFetcher
+from . fetchers import TwoCheckoutFetcher, WePayFetcher
 from members.serializers import PaidMembershipSerializer
 import requests
 import logging
@@ -10,28 +10,32 @@ import sys
 __author__ = 'adrian'
 
 
+def is_subset_dict(larger, smaller):
+    for key, value in smaller.items():
+        if not key in larger: return False
+        elif larger[key] != smaller[key]: return False
+    return True
+
+
 class Command(BaseCommand):
 
     help = "Meant to be run on a server other than the web server, this ETLs financials from various sources."
 
-    def handle(self, *args, **options):
+    URL = "http://xerocraft-django.herokuapp.com/members/paidmemberships/"  # IMPORTANT: Set URL back to production
+    #URL = "http://localhost:8000/members/paidmemberships/"
+
+    auth_headers = None
+
+    def handle_fetcher(self, fetcher):
 
         session = requests.Session()
         dotcount = 0
-        fetcher = TwoCheckoutFetcher()
-
-        URL = "http://xerocraft-django.herokuapp.com/members/paidmemberships/"  # IMPORTANT: Set URL back to production
-        #URL = "http://localhost:8000/members/paidmemberships/"
-
-        print("Will push data to {}".format(URL))
-        rest_token = input("REST API token: ")
-        auth_headers = {'Authorization': "Token " + rest_token}
 
         for pm in fetcher.generate_payments():
 
             # See if the PaidMembership has previously been sent:
             get_params = {'payment_method': pm.payment_method, 'ctrlid': pm.ctrlid}
-            response = session.get(URL, params=get_params, headers=auth_headers)
+            response = session.get(self.URL, params=get_params, headers=self.auth_headers)
             matchcount = int(response.json()['count'])
             if matchcount == 0:
                 id = None
@@ -42,26 +46,40 @@ class Command(BaseCommand):
                 raise AssertionError("Too many matches for method %s ctrlid %s" % (pm.payment_method, pm.ctrlid))
 
             # Either POST or PUT depending on whether it already exists
-            twocodata = PaidMembershipSerializer(pm).data
+            fetched_data = PaidMembershipSerializer(pm).data
             if id is None:
                 # The information is not yet on the website, so add it.
-                response = session.post(URL, twocodata, headers=auth_headers)
+                response = session.post(self.URL, fetched_data, headers=self.auth_headers)
                 progchar = "+"  # Added
             else:
                 # Are all the items from 2checkout already on the website?
                 djangodata = response.json()['results'][0]
-                if all(item in djangodata for item in twocodata):
+                fetched_data['id'] = djangodata['id']  # So subset comparison can be made, below:
+                if is_subset_dict(djangodata, fetched_data):
                     # Yes, so don't do anything with the 2checkout data.
-                    progchar = "I"  # Ignored
+                    progchar = "="  # Equal so no add or update required.
                 else:
                     # No, so update the data on the website.
-                    response = session.put("{}{}/".format(URL, id), twocodata, headers=auth_headers)
+                    response = session.put("{}{}/".format(self.URL, id), fetched_data, headers=self.auth_headers)
                     progchar = "U"  # Updated
             if response.status_code >= 300:
                 progchar = "E"  # Error
 
-            print(progchar, end='')
+            print(progchar, end='')  # Progress indicator
             dotcount += 1
             if dotcount % 50 == 0:
                 print(" {}".format(dotcount))
             sys.stdout.flush()
+        print("")
+
+    def handle(self, *args, **options):
+
+        print("Will push data to {}".format(self.URL))
+        rest_token = input("REST API token: ")
+        print("")
+        self.auth_headers = {'Authorization': "Token " + rest_token}
+
+        fetchers = [WePayFetcher(), TwoCheckoutFetcher()]
+        for fetcher in fetchers:
+            self.handle_fetcher(fetcher)
+
--- members/management/commands/fetchers.py
+++ members/management/commands/fetchers.py
@@ -1,13 +1,17 @@
 import abc
 import twocheckout
 from decimal import Decimal
-from dateutil import parser
 from members.models import PaidMembership
 from dateutil.relativedelta import relativedelta
 from dateutil import parser
+from datetime import date
+import time
+import requests
 
 
-class Fetcher(object):
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+class Fetcher(object):  # The "ET" in "ETL"
 
     __metalass__ = abc.ABCMeta
 
@@ -16,6 +20,8 @@ class Fetcher(object):
         """Yields an unsaved PaidMembership instance"""
         return
 
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 class TwoCheckoutFetcher(Fetcher):
 
@@ -43,7 +49,9 @@ class TwoCheckoutFetcher(Fetcher):
 
             # if invoice.invoice_id in invoices_to_skip: continue
 
-            if not invoice.payout_status.startswith("Paid"):
+            paid_status = invoice.payout_status.startswith("Paid")
+            captured_status = invoice.payout_status.startswith("Captured")
+            if not (paid_status or captured_status):
                 # See http://help.2checkout.com/articles/Knowledge_Article/Payout-Status
                 continue
 
@@ -88,3 +96,78 @@ class TwoCheckoutFetcher(Fetcher):
             page_num += 1
 
 
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+def date2timestamp(date):
+    return int(time.mktime((date.year, date.month, date.day, 0, 0, 0, 0, 0, 0)))
+
+
+class WePayFetcher(Fetcher):
+
+    session = requests.Session()
+
+    limit = 1000  # The max number of checkins returned per find.
+
+    def generate_from_checkouts(self, checkouts):
+        assert len(checkouts) < self.limit
+        for checkout in checkouts:
+
+            if not checkout['state'].startswith("captured"):
+                continue
+
+            desc = checkout['short_description']
+            if desc.startswith("One Month Membership"): months = 1
+            elif desc.startswith("Three Month Membership"): months = 3
+            elif desc.startswith("Six Month Membership"): months = 6
+            elif desc.startswith("Recurring Payment to Dues-Paying Member"): months = 1
+            elif desc.startswith("Payment to Dues-Paying Member ONE-TIME"): months = 1
+            else:
+                if desc.endswith("Event Payment"): continue
+                if desc.startswith("Recurring Payment to Donation"): continue
+                if desc.startswith("Payment to Donation at"): continue
+                print(desc)
+                continue
+
+            if desc.endswith("+ 1 family member"): family = 1
+            elif desc.endswith("+ 2 family member"): family = 2
+            elif desc.endswith("+ 3 family member"): family = 3
+            elif desc.endswith("+ 4 family member"): family = 4
+            elif desc.endswith("+ 5 family member"): family = 5
+            elif desc.endswith("+ 6 family member"): family = 6
+            else: family = 0
+
+            pm = PaidMembership()
+            pm.payment_method = PaidMembership.PAID_BY_WEPAY
+            pm.ctrlid = checkout['checkout_id']
+            pm.payer_email = checkout['payer_email']
+            pm.payer_name = checkout['payer_name']
+            pm.family_count = family
+            pm.payment_date = date.fromtimestamp(int(checkout['create_time']))
+            pm.start_date = pm.payment_date
+            pm.end_date = pm.start_date + relativedelta(months=months, days=-1)
+            pm.paid_by_member = Decimal(checkout['gross'])  # Docs: "The total dollar amount paid by the payer"
+            pm.processing_fee = Decimal(checkout['fee']) + Decimal(checkout['app_fee'])
+            yield pm
+
+    def generate_payments(self):
+        accounts = input("WePay Accounts: ").split()
+        rest_token = input("WePay Token: ")  # So far, same token works for all accts.
+        auth_headers = {'Authorization': "Bearer " + rest_token}
+
+        for account in accounts:
+            URL = "https://wepayapi.com/v2/checkout/find"
+
+            window_start = date(2013, 12, 1)
+            while window_start < date.today():
+                window_start = window_start + relativedelta(months=+1)
+                window_end = window_start + relativedelta(months=+1)
+                post_data = {
+                    'account_id': account,
+                    'start_time': str(date2timestamp(window_start)),
+                    'end_time': str(date2timestamp(window_end)),
+                    'limit': str(self.limit)
+                }
+                response = self.session.post(URL, post_data, headers=auth_headers)
+                checkouts = response.json()
+                for pm in self.generate_from_checkouts(checkouts):
+                    yield pm
