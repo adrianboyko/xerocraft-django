ac704c9f494b40b62bab27f4c6399218fa5ebbe1
adrian <AdrianBoyko@gmail.com>
2016-04-05 12:23:41 -0700

    Two minor fixes from pycharm code inspection.

--- inventory/tests.py
+++ inventory/tests.py
@@ -4,13 +4,14 @@ from django.utils import timezone
 from inventory.models import ParkingPermit, PermitScan, Location
 from django.contrib.auth.models import User
 
+
 class TestStringReps(TestCase):
 
     def setUp(self):
         user = User.objects.create(username='fake1', first_name="Andrew", last_name="Baker", password="fake1")
         self.member = user.member
         permit = ParkingPermit.objects.create(owner=self.member, short_desc="Test")
-        self.permit = permit;
+        self.permit = permit
         location = Location.objects.create()
         self.location = location
         scan = PermitScan.objects.create(permit=permit, where=location, when=timezone.now())
