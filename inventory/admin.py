bdd67ce64d76fdec20a431420b6d92d89f492bf0
adrian <AdrianBoyko@gmail.com>
2016-05-28 00:41:08 -0700

    (1) added versioning, (2) changes per PyLint, (3) raw id fields

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,12 @@
+
+# Standard
+
+# Third Party
 from django.contrib import admin
-from .models import PermitScan, PermitRenewal, ParkingPermit, Location
+from reversion.admin import VersionAdmin
+
+# Local
+from inventory.models import PermitScan, PermitRenewal, ParkingPermit, Location
 
 
 class PermitRenewalInline(admin.TabularInline):
@@ -13,16 +20,24 @@ class PermitScanInline(admin.TabularInline):
 
 
 @admin.register(ParkingPermit)
-class ParkingPermitAdmin(admin.ModelAdmin):
+class ParkingPermitAdmin(VersionAdmin):
     list_display = ['short_desc', 'owner', 'created', 'ok_to_move', 'is_in_inventoried_space']
     fields = ['short_desc', 'owner', 'created', 'ok_to_move', 'is_in_inventoried_space']
     readonly_fields = ['created']
     inlines = [PermitRenewalInline, PermitScanInline]
+    raw_id_fields = ['owner']
 
 
 @admin.register(PermitScan)
-class PermitScanAdmin(admin.ModelAdmin):
+class PermitScanAdmin(VersionAdmin):
     list_display = ['pk', 'when', 'permit', 'where']
 
-admin.site.register(Location)
-admin.site.register(PermitRenewal)
+
+@admin.register(Location)
+class LocationAdmin(VersionAdmin):
+    pass
+
+
+@admin.register(PermitRenewal)
+class PermitRenewalAdmin(VersionAdmin):
+    pass
