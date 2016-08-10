535ad7d9adecc864b38bcb03653c571cc9212b6c
adrian <AdrianBoyko@gmail.com>
2016-08-10 10:22:10 -0700

    Commented out some of the social auth stuff since it's not currently being used.

--- xerocraft/settings.py
+++ xerocraft/settings.py
@@ -62,16 +62,17 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Social auth would work inside AND outside if we replace reverse proxy inside XC with Heroku's SSL.
 # That said, I'm not sure that access outside Xerocraft should be allowed at all.
 # BTW, one Stack Exchange comment blames Heroku: "This is because heroku fails to pass the headers"
-SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['GOOGLE_OAUTH2_KEY']
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['GOOGLE_OAUTH2_SECRET']
+# SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
-SOCIAL_AUTH_TWITTER_KEY = os.environ['TWITTER_KEY']
-SOCIAL_AUTH_TWITTER_SECRET = os.environ['TWITTER_SECRET']
+# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ['GOOGLE_OAUTH2_KEY']
+# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ['GOOGLE_OAUTH2_SECRET']
 
-SOCIAL_AUTH_FACEBOOK_KEY =  os.environ['FACEBOOK_KEY']
-SOCIAL_AUTH_FACEBOOK_SECRET =  os.environ['FACEBOOK_SECRET']
+# SOCIAL_AUTH_TWITTER_KEY = os.environ['TWITTER_KEY']
+# SOCIAL_AUTH_TWITTER_SECRET = os.environ['TWITTER_SECRET']
+
+# SOCIAL_AUTH_FACEBOOK_KEY =  os.environ['FACEBOOK_KEY']
+# SOCIAL_AUTH_FACEBOOK_SECRET =  os.environ['FACEBOOK_SECRET']
 
 # Application definition
 INSTALLED_APPS = (
@@ -127,9 +128,9 @@ SESSION_SAVE_EVERY_REQUEST = True
 AUTHENTICATION_BACKENDS = (
     'xerocraft.authenticators.CaseInsensitiveModelBackend',
     'xerocraft.authenticators.XerocraftBackend',
-    'social.backends.facebook.FacebookOAuth2',
-    'social.backends.google.GoogleOAuth2',
-    'social.backends.twitter.TwitterOAuth',
+    # 'social.backends.facebook.FacebookOAuth2',
+    # 'social.backends.google.GoogleOAuth2',
+    # 'social.backends.twitter.TwitterOAuth',
 )
 if ISDEVHOST:
     AUTHENTICATION_BACKENDS += (
@@ -158,8 +159,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.core.context_processors.request',
-                'social.apps.django_app.context_processors.backends',
-                'social.apps.django_app.context_processors.login_redirect',
+                # 'social.apps.django_app.context_processors.backends',
+                # 'social.apps.django_app.context_processors.login_redirect',
             ],
         },
     },
