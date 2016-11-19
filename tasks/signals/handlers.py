9b739a1a51d44dfcf35795aaca8f16f1fbcd1af5
adrian <AdrianBoyko@gmail.com>
2016-11-18 17:52:18 -0700

    Added notifications for staffing changes, based on claims.

--- tasks/signals/handlers.py
+++ tasks/signals/handlers.py
@@ -1,11 +1,18 @@
-from django.db.models.signals import post_save
+# Standard
+import logging
+
+# Third Party
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
+
+# Local
 from members.models import Member, Tag, Tagging
-from tasks.models import Worker
+from tasks.models import Worker, Claim
+import members.notifications as notifications
 
 __author__ = 'Adrian'
+
+logger = logging.getLogger("tasks")
 
 
 @receiver(post_save, sender=Tagging)
@@ -21,3 +28,29 @@ def act_on_new_tag(sender, **kwargs):
 def create_default_worker(sender, **kwargs):
     if kwargs.get('created', True):
         w,_ = Worker.objects.get_or_create(member=kwargs.get('instance'))
+
+
+@receiver(pre_save, sender=Claim)
+def staffing_update_notification(sender, **kwargs):
+    try:
+        if kwargs.get('created', True):
+            claim = kwargs.get('instance')  # type: Claim
+            message = None
+            if claim.status in [Claim.STAT_UNINTERESTED, Claim.STAT_ABANDONED, Claim.STAT_EXPIRED]:
+                message = "{0} will NOT work '{1}' on {2:%a %m/%d}".format(
+                    claim.claiming_member.friendly_name,
+                    claim.claimed_task.short_desc,
+                    claim.claimed_task.scheduled_date
+                )
+            if claim.status == Claim.STAT_CURRENT and claim.date_verified is not None:
+                message = "{0} WILL work '{1}' on {2:%a %m/%d}".format(
+                    claim.claiming_member.friendly_name,
+                    claim.claimed_task.short_desc,
+                    claim.claimed_task.scheduled_date
+                )
+            if message is not None:
+                recipient = Member.objects.get(auth_user__username='adrianb')
+                notifications.notify(recipient, "Staffing Update", message)
+
+    except Exception as e:
+        logger.error("Problem sending staffing update.")
