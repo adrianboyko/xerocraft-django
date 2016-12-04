f12dc3afa7be29e2b208791c138c01667b52ce9d
adrian <AdrianBoyko@gmail.com>
2016-12-03 23:39:55 -0700

    Beginning maintenance_nag which is a VisitEvent post_save handler.

--- tasks/signals/handlers.py
+++ tasks/signals/handlers.py
@@ -1,13 +1,14 @@
 # Standard
 import logging
+from datetime import date
 
 # Third Party
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
 # Local
-from members.models import Member, Tag, Tagging
-from tasks.models import Worker, Claim
+from members.models import Member, Tagging, VisitEvent, Pushover
+from tasks.models import Task, Worker, Claim
 import members.notifications as notifications
 
 __author__ = 'Adrian'
@@ -54,3 +55,56 @@ def staffing_update_notification(sender, **kwargs):
 
     except Exception as e:
         logger.error("Problem sending staffing update.")
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# VISIT
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+@receiver(post_save, sender=VisitEvent)
+def maintenance_nag(sender, **kwargs):
+    try:
+        if kwargs.get('created', True):
+            visit = kwargs.get('instance')  # type: VisitEvent
+
+            # We're only interested in arrivals
+            if visit.event_type != VisitEvent.EVT_ARRIVAL:
+                return
+
+            # Only act on a member's first visit of the day.
+            num_visits_today = VisitEvent.objects.filter(
+                who=visit.who,
+                type=VisitEvent.EVT_ARRIVAL,
+                when__gte=date.today(),
+            ).count()
+            if num_visits_today > 1:
+                return
+
+            # This gets tasks that are scheduled like maintenance tasks.
+            # I.e. those that need to be done every X days, but can slide.
+            tasks = Task.objects.filter(
+                eligible_claimants=visit.who,
+                scheduled_date=date.today(),
+                status=Task.STAT_ACTIVE,
+                should_nag=True,
+                recurring_task_template__repeat_interval__isnull=False,
+                recurring_task_template__missed_date_action=Task.MDA_SLIDE_SELF_AND_LATER,
+            )
+
+            if len(tasks)>0 and Pushover.getkey(visit.who) is None:
+                logger.info(
+                    "Wanted to notify %s of task(s) but Pushover key not available.",
+                    visit.who.friendly_name
+                )
+                return
+
+            for task in tasks:  # type: Task
+                pass
+                # TODO
+                # message = "..."
+                # notifications.notify(visit.who, task.short_desc, message)
+
+    except Exception as e:
+        # Makes sure that problems here do not prevent the visit event from being saved!
+        logger.error("Problem in maintenance_nag: %s", str(e))
+
