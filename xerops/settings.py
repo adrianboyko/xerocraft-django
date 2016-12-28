785bb68c5a4c9ba9c2ca729fe955d38972623cc8
adrian <AdrianBoyko@gmail.com>
2016-12-27 18:29:07 -0700

    Production test done.

--- xerops/settings.py
+++ xerops/settings.py
@@ -52,7 +52,7 @@ SECRET_KEY = os.environ['XEROPS_DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if ISDEVHOST else False
-DEBUG = True  # TEMPORARY FOR PRODUCTION TESTING!!!!!!!!!!
+
 ALLOWED_HOSTS = (
     'xerocraft-django.herokuapp.com',
     'xis.xerocraft.us',
