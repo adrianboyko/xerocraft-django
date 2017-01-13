a796bbff0b1e89ff17018399f64d3e36e6ded61f
adrian <AdrianBoyko@gmail.com>
2017-01-13 12:01:30 -0700

    Added error counting and removed dead code.

--- books/management/commands/generatejournal.py
+++ books/management/commands/generatejournal.py
@@ -20,21 +20,6 @@ class Command(BaseCommand):
 
     help = "(Re)generates the journal from existing transactions."
 
-    def process_journalliners(journaler: Journaler):
-        related_objects = [
-            f for f in journaler._meta.get_fields()
-              if (f.one_to_many or f.one_to_one)
-              and f.auto_created
-              and not f.concrete
-        ]
-        link_names = [rel.get_accessor_name() for rel in related_objects]
-        for link_name in link_names:
-            children = getattr(journaler, link_name).all()
-            for child in children:
-                # The children include Notes and other objs of no interest here.
-                if isinstance(child, JournalLiner):
-                    child.create_journalentry_lineitems(journaler)
-
     def handle(self, *args, **options):
         print("Deleting unfrozen journal entries... ", end="")
         sys.stdout.flush()
@@ -55,18 +40,20 @@ class Command(BaseCommand):
 
         grand_total_debits = Decimal(0.0)
         grand_total_credits = Decimal(0.0)
+        err_count = 0
         print("\nVerifying that individual journal entries balance...")
         for je in JournalEntry.objects.all():  # type: JournalEntry
             debits, credits = je.debits_and_credits()
             grand_total_debits += debits
             grand_total_credits += credits
             if debits != credits:
+                err_count += 1
                 print("\nJournal Entry #{} doesn't balance.".format(je.pk))
                 print("   http://localhost:8000{}".format(je.source_url))  # TODO: Get base from sites app?
                 for li in je.journalentrylineitem_set.all():
                     print("   {}".format(str(li)))
-        print("\nDone.")
-        print("")
+        print("\nDone.\n")
+        print("        Error count: {}", err_count)
         print("Grand total credits: {}", grand_total_credits)
         print(" Grand total debits: {}", grand_total_debits)
         print("         Difference: {}", grand_total_credits - grand_total_debits)
