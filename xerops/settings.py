1433ae72872621e4775ff86c4a7803ae4a19f402
adrian <AdrianBoyko@gmail.com>
2017-01-23 10:53:54 -0700

    Turning DEBUG off.

--- xerops/settings.py
+++ xerops/settings.py
@@ -52,7 +52,6 @@ SECRET_KEY = os.environ['XEROPS_DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if ISDEVHOST else False
-DEBUG = True  # TEMPORARY!
 
 ALLOWED_HOSTS = (
     'xerocraft-django.herokuapp.com',
