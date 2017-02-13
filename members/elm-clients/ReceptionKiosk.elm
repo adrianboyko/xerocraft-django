b2b49123832667c100bc6e72ca010dd95c1c4c63
adrian <AdrianBoyko@gmail.com>
2017-02-12 19:39:16 -0700

    Added basic forward naviation and some graphics & style. Still WIP.

--- members/elm-clients/ReceptionKiosk.elm
+++ members/elm-clients/ReceptionKiosk.elm
@@ -1,7 +1,12 @@
 module ReceptionKiosk exposing (..)
 
-import Html exposing (Html, Attribute, a, div, table, tr, td, th, text, span, button, br, p)
+import Html exposing (Html, Attribute, a, div, text, span, button, br, p, img)
+import Html.Attributes exposing (style, src)
 import Regex exposing (regex)
+
+import Material
+import Material.Button as Button
+import Material.Options as Options exposing (css)
 
 -----------------------------------------------------------------------------
 -- MAIN
@@ -24,6 +29,7 @@ replaceAll : String -> String -> String -> String
 replaceAll theString oldSub newSub =
   Regex.replace Regex.All (regex oldSub ) (\_ -> newSub) theString
 
+
 -----------------------------------------------------------------------------
 -- MODEL
 -----------------------------------------------------------------------------
@@ -32,14 +38,16 @@ replaceAll theString oldSub newSub =
 type alias Flags =
   { csrfToken: String
   , orgName: String
+  , bannerTopUrl: String
+  , bannerBottomUrl: String
   }
 
 type Step
   = Welcome
-  | HaveAcct
+  | HaveAcctQ
   | CheckIn
   | LetsCreate
-  | UserIdAndPw
+  | ChooseIdAndPw
   | HowDidYouHear
   | Waiver
   | Rules
@@ -50,41 +58,63 @@ type Step
 type alias Model =
   { csrfToken: String
   , orgName: String
+  , bannerTopUrl: String
+  , bannerBottomUrl: String
   , step: Step
+  , mdl: Material.Model
   }
 
-type Msg = Spam
-
 init : Flags -> (Model, Cmd Msg)
-init {csrfToken, orgName} =
-  ( Model csrfToken orgName Welcome, Cmd.none)
+init f =
+  let model =
+    Model f.csrfToken f.orgName f.bannerTopUrl f.bannerBottomUrl Welcome Material.model
+  in
+    (model, Cmd.none)
 
 
 -----------------------------------------------------------------------------
 -- UPDATE
 -----------------------------------------------------------------------------
 
+type Msg
+  = Mdl (Material.Msg Msg)  -- For elm-mdl
+  | Segue Step
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
   case action of
 
-    Spam -> (model, Cmd.none)
+    Mdl msg2 ->
+      Material.update Mdl msg2 model
+
+    Segue nextStep ->
+      ({model | step=nextStep }, Cmd.none)
 
 
 -----------------------------------------------------------------------------
 -- VIEW
 -----------------------------------------------------------------------------
 
-stepDialog: Model -> String -> String -> Html Msg
-stepDialog model inTitle inSubtitle =
+type alias ButtonSpec = { title : String, segue: Step }
+
+dialogButton : Model -> ButtonSpec -> Html Msg
+dialogButton model buttonSpec =
+  Button.render Mdl [0] model.mdl
+    ([ Button.raised, Options.onClick (Segue buttonSpec.segue)]++dlgButtonCss)
+    [ text buttonSpec.title ]
+
+stepDialog: Model -> String -> String -> List ButtonSpec -> Html Msg
+stepDialog model inTitle inSubtitle buttonSpecs =
   let
     title = replaceAll inTitle "ORGNAME" model.orgName
     subtitle = replaceAll inSubtitle "ORGNAME" model.orgName
   in
-    div []
-      [ text title
-      , br [] []
-      , text subtitle
+    div [stepDialogStyle]
+      [ img [src model.bannerTopUrl, bannerTopStyle] []
+      , p [stepTitleStyle] [text title]
+      , p [stepSubtitleStyle] [text subtitle]
+      , div [] (List.map (dialogButton model) buttonSpecs)
+      , img [src model.bannerBottomUrl, bannerBottomStyle] []
       ]
 
 view : Model -> Html Msg
@@ -93,58 +123,75 @@ view model =
 
     Welcome ->
       stepDialog model
-        "Welcome to ORGNAME!"
+        "Welcome!"
         "Is this your first visit?"
+        [ ButtonSpec "Yes" HaveAcctQ
+        , ButtonSpec "No" CheckIn
+        ]
 
-    HaveAcct ->
+    HaveAcctQ ->
       stepDialog model
         "Great!"
         "Do you already have an account here or on our website?"
+        [ ButtonSpec "Yes, I do" CheckIn
+        , ButtonSpec "No, I don't" LetsCreate
+        -- TODO: How about a "I don't know" button, here?
+        ]
 
     CheckIn ->
       stepDialog model
         "Please Check In"
         "Enter your userid or email address:"
+        [ButtonSpec "OK" Waiver]
 
     LetsCreate ->
       stepDialog model
         "Let's Create One!"
         "Please tell us about yourself:"
+        [ButtonSpec "OK" ChooseIdAndPw]
 
-    UserIdAndPw ->
+    ChooseIdAndPw ->
       stepDialog model
         "Id & Password"
         "Please chooose a userid and password for your account:"
+        [ButtonSpec "OK" HowDidYouHear]
 
     HowDidYouHear ->
       stepDialog model
         "Just Wondering"
-        "How did you hear about ORGNAME"
+        "How did you hear about us?"
+        [ButtonSpec "OK" Waiver]
 
     Waiver ->
       stepDialog model
         "Waiver"
-        "Please read the waiver and sign in the box to accept."
+        "Please read the waiver and sign in the box."
+        -- TODO: How about a "Clear" choice here?
+        [ButtonSpec "Accept" Rules]
 
     Rules ->
       stepDialog model
         "Rules"
         "Please read the rules and check the box to agree."
+        [ButtonSpec "I Agree" Activity]
 
     Activity ->
       stepDialog model
         "Today's Activity?"
         "Let us know what you'll be doing:"
+        [ButtonSpec "OK" SupportUs]
 
     SupportUs ->
       stepDialog model
-        "Support ORGNAME"
+        "Please Support Us!"
         "{TODO}"
+        [ButtonSpec "OK" Done]
 
     Done ->
       stepDialog model
         "You're Checked In"
         "Have fun!"
+        [ButtonSpec "Yay!" Welcome]
 
 
 -----------------------------------------------------------------------------
@@ -156,3 +203,58 @@ subscriptions model =
   Sub.batch
     [
     ]
+
+-----------------------------------------------------------------------------
+-- STYLES
+-----------------------------------------------------------------------------
+
+(=>) = (,)
+
+dialogWidth = "640px"
+
+stepDialogStyle = style
+  [ "font-family" => "Roboto Condensed, Arial, Helvetica"
+  , "text-align" => "center"
+  , "margin-left" => "auto"
+  , "margin-right" => "auto"
+  , "margin-top" => "10%"
+  , "width" => dialogWidth
+  , "border" => "1px solid #bbbbbb"
+  , "background-color" => "white"
+  , "padding" => "0px"
+  ]
+
+stepTitleStyle = style
+  [ "font-size" => "22pt"
+  , "margin-left" => "auto"
+  , "margin-right" => "auto"
+  , "margin-top" => "50px"
+  , "margin-bottom" => "0.25em"
+  ]
+
+stepSubtitleStyle = style
+  [ "font-size" => "16pt"
+  , "margin-left" => "auto"
+  , "margin-right" => "auto"
+  , "margin-top" => "0"
+  ]
+
+bannerTopStyle = style
+  [ "width" => dialogWidth
+  , "margin" => "0"
+  ]
+
+bannerBottomStyle = style
+  [ "width" => dialogWidth
+  , "margin-top" => "50px"
+  ]
+
+dlgButtonCss =
+  [ css "margin-left" "10px"
+  , css "margin-right" "10px"
+  , css "padding-top" "25px"
+  , css "padding-bottom" "55px"
+  , css "padding-left" "30px"
+  , css "padding-right" "30px"
+  , css "font-size" "14pt"
+  ]
