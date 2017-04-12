48958fb9e71692b87a39a5c78099f3e12e6c9101
adrian <AdrianBoyko@gmail.com>
2017-04-12 02:08:35 -0700

    Added is_nth_day(...)

--- abutils/time.py
+++ abutils/time.py
@@ -1,5 +1,6 @@
 # Standard
 from datetime import date, timedelta
+import calendar
 
 # Third-Party
 
@@ -9,3 +10,12 @@ from datetime import date, timedelta
 def is_last_day_of_month(curr_date: date):
     next_day = curr_date + timedelta(days=1)  # type: date
     return next_day.month != curr_date.month
+
+
+# E.g. is_nth_day(date.today(), 3, 3) returns true if today is the 3rd Thursday of the month.
+# Note that Monday is 0, Tuesday is 1, etc...
+def is_nth_day(date_in_question: date, nth: int, daynum: int) -> bool:
+    return date_in_question == calendar.Calendar(nth).monthdatescalendar(
+        date_in_question.year,
+        date_in_question.month
+    )[daynum][0]
