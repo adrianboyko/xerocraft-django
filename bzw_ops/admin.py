2c92d9825ed290007effa77fe4959490ac3cfde0
adrian <AdrianBoyko@gmail.com>
2017-09-14 11:35:09 -0700

    Initial functionality for specifying "time blocks". This is like "hours of operation" but with more information to distinguish "open to everybody" vs "open for paying members" time blocks.

--- bzw_ops/admin.py
+++ bzw_ops/admin.py
@@ -1,9 +1,21 @@
 
+# Standard
 
+# Third Party
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
+# Local
+from bzw_ops.models import TimeBlockType, TimeBlock
+from abutils.time import (
+    days_of_week_str,
+    duration_single_unit_str,
+    ordinals_of_month_str,
+)
+
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 class CustomUserAdmin(UserAdmin):
     def __init__(self, *args, **kwargs):
@@ -18,5 +30,70 @@ class CustomUserAdmin(UserAdmin):
     # def some_function(self, obj):
     #     return obj.another_model_set.count()
 
+
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
+
+
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+
+@admin.register(TimeBlock)
+class TimeBlockAdmin(admin.ModelAdmin):
+
+    def fmt_ords_str(self, obj) -> str:
+        return ordinals_of_month_str(obj)
+
+    def fmt_days_str(self, obj) -> str:
+        return days_of_week_str(obj)
+
+    def fmt_dur_str(self, obj) -> str:
+        return duration_single_unit_str(obj.duration)
+
+    def fmt_types(self, obj) -> str:
+        types = [bt.name for bt in obj.types.all()]
+        return ", ".join(types)
+
+    list_display = [
+        'pk',
+        'fmt_ords_str',
+        'fmt_days_str',
+        'start_time',
+        'fmt_dur_str',
+        'fmt_types'
+    ]
+
+    fields = [
+        (
+            'first',
+            'second',
+            'third',
+            'fourth',
+            'last',
+            'every',
+        ),
+        (
+            'monday',
+            'tuesday',
+            'wednesday',
+            'thursday',
+            'friday',
+            'saturday',
+            'sunday',
+        ),
+        'start_time',
+        'duration',
+        'types',
+    ]
+
+
+TimeBlockAdmin.fmt_ords_str.short_description = "Ordinals"
+TimeBlockAdmin.fmt_days_str.short_description = "Days"
+TimeBlockAdmin.fmt_dur_str.short_description = "Duration"
+TimeBlockAdmin.fmt_types.short_description = "Activities"
+
+
+@admin.register(TimeBlockType)
+class TimeBlockTypeAdmin(admin.ModelAdmin):
+    pass
+
---
+++ bzw_ops/models.py
@@ -0,0 +1,69 @@
+# pylint: disable=E128
+
+# Standard
+from decimal import Decimal
+from datetime import datetime
+from typing import Union, List, Set, Dict, Type
+
+# Third-party
+from django.db import models
+from django.core.exceptions import ValidationError
+
+# Local
+from abutils.time import (
+    days_of_week_str,
+    duration_single_unit_str,
+    ordinals_of_month_str
+)
+
+
+class TimeBlockType(models.Model):
+
+    name = models.CharField(max_length=32, null=False, blank=False,
+        help_text="A short name for the time block type.")
+
+    description = models.TextField(max_length=2048, null=False, blank=False,
+        help_text="A longer description for the time block type.")
+
+    def __str__(self):
+        return self.name
+
+
+class TimeBlock(models.Model):
+
+    start_time = models.TimeField(null=True, blank=True,
+        help_text="The time at which the time block begins.")
+
+    duration = models.DurationField(null=True, blank=True,
+        help_text="The duration/length of the time block (HH:MM:SS).")
+
+    # Position in month:
+    first = models.BooleanField(default=False)
+    second = models.BooleanField(default=False)
+    third = models.BooleanField(default=False)
+    fourth = models.BooleanField(default=False)
+    last = models.BooleanField(default=False)
+    every = models.BooleanField(default=False)
+
+    # Day of week:
+    monday = models.BooleanField(default=False)
+    tuesday = models.BooleanField(default=False)
+    wednesday = models.BooleanField(default=False)
+    thursday = models.BooleanField(default=False)
+    friday = models.BooleanField(default=False)
+    saturday = models.BooleanField(default=False)
+    sunday = models.BooleanField(default=False)
+
+    types = models.ManyToManyField(TimeBlockType)
+
+    def clean(self):
+        particular = self.first or self.second or self.third or self.fourth or self.last
+        if self.every and particular:
+            msg = "Doesn't make sense to combine 'Every' with some other ordinal."
+            raise ValidationError(msg)
+
+    def __str__(self):
+        ords = ordinals_of_month_str(self)  # type: str
+        days = days_of_week_str(self)  # type: str
+        dur = duration_single_unit_str(self.duration)  # type: str
+        return "{} / {} at {} for {}".format(ords, days, self.start_time, dur)
