196af63be0a97122d855fe905f5d935e2dd9d4b0
adrian <adrianboyko@gmail.com>
2018-01-03 15:43:47 -0700

    Removed implicit filtering of Tasks, Work, and Claims lists. Now allowing read-only accesss for unauthenticated users.

--- tasks/restapi/views.py
+++ tasks/restapi/views.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Third Party
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.settings import api_settings
 
 # Local
@@ -25,7 +25,7 @@ from xis.utils import user_is_kiosk
 class ClaimViewSet(viewsets.ModelViewSet):
     queryset = tm.Claim.objects.all().order_by('id')
     serializer_class = ts.ClaimSerializer
-    permission_classes = [IsAuthenticated, tp.ClaimPermission]
+    permission_classes = [IsAuthenticatedOrReadOnly, tp.ClaimPermission]
     authentication_classes = [
         ta.NagAuthentication,
     ] + api_settings.DEFAULT_AUTHENTICATION_CLASSES
@@ -33,17 +33,12 @@ class ClaimViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user  # type: User
-        memb = user.member  # type: Member
 
         if user_is_kiosk(self.request):
             return tm.Claim.objects.all().order_by('id')
         else:
-            if self.action is "list":
-                # Filter to show only memb's current/future claims.
-                today = datetime.today()
-                return tm.Claim.objects.filter(claiming_member=memb, claimed_task__scheduled_date__gte=today).order_by('id')
-            else:
-                return tm.Claim.objects.all().order_by('id')
+            # We used to trim down the set for non-kiosk users, but won't, for now.
+            return tm.Claim.objects.all().order_by('id')
 
 
 # ---------------------------------------------------------------------------
@@ -53,7 +48,7 @@ class ClaimViewSet(viewsets.ModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = tm.Task.objects.all().order_by('id')
     serializer_class = ts.TaskSerializer
-    permission_classes = [IsAuthenticated, tp.TaskPermission]
+    permission_classes = [IsAuthenticatedOrReadOnly, tp.TaskPermission]
     authentication_classes = [
         ta.NagAuthentication,
     ] + api_settings.DEFAULT_AUTHENTICATION_CLASSES
@@ -61,18 +56,12 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user  # type: User
-        memb = user.member  # type: Member
 
         if user_is_kiosk(self.request):
             return tm.Task.objects.all().order_by('id')
         else:
-            # Limit what other authenticated users can see.
-            if self.action is "list":
-                # Filter to show only memb's current/future tasks.
-                today = datetime.today()
-                return tm.Task.objects.filter(owner=memb, scheduled_date__gte=today)
-            else:
-                return tm.Task.objects.all().order_by('id')
+            # We used to trim down the set for non-kiosk users, but won't, for now.
+            return tm.Task.objects.all().order_by('id')
 
 
 # ---------------------------------------------------------------------------
@@ -82,37 +71,27 @@ class TaskViewSet(viewsets.ModelViewSet):
 class WorkViewSet(viewsets.ModelViewSet):
     queryset = tm.Work.objects.all().order_by('id')
     serializer_class = ts.WorkSerializer
-    permission_classes = [IsAuthenticated, tp.WorkPermission]
+    permission_classes = [IsAuthenticatedOrReadOnly, tp.WorkPermission]
     filter_class = filt.WorkFilter
 
     def get_queryset(self):
-        memb = self.request.user.member
 
         if user_is_kiosk(self.request):
             return tm.Work.objects.all().order_by('id')
         else:
-            if self.action is "list":
-                # Filter to show only memb's work.
-                today = datetime.today()
-                return tm.Work.objects.filter(claim__claiming_member=memb)
-            else:
-                return tm.Work.objects.all().order_by('id')
+            # We used to trim down the set for non-kiosk users, but won't, for now.
+            return tm.Work.objects.all().order_by('id')
 
 
 class WorkNoteViewSet(viewsets.ModelViewSet):
     queryset = tm.WorkNote.objects.all().order_by('id')
     serializer_class = ts.WorkNoteSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        memb = self.request.user.member
 
         if user_is_kiosk(self.request):
             return tm.WorkNote.objects.all().order_by('id')
         else:
-            if self.action is "list":
-                # Filter to show only notes on member's works.
-                today = datetime.today()
-                return tm.WorkNote.objects.filter(work__claim__claiming_member=memb)
-            else:
-                return tm.WorkNote.objects.all().order_by('id')
+            # We used to trim down the set for non-kiosk users, but won't, for now.
+            return tm.WorkNote.objects.all().order_by('id')
