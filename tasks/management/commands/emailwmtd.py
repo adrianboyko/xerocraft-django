9e540ff42cbb73b0748362a183b2616caac14f1d
adrian <adrianboyko@gmail.com>
2018-01-09 10:29:41 -0700

    Bug fix and type hints.

--- tasks/management/commands/emailwmtd.py
+++ tasks/management/commands/emailwmtd.py
@@ -2,6 +2,7 @@
 # Standard
 import datetime
 import logging
+from typing import List, Tuple
 
 # Third Party
 from django.core.management.base import BaseCommand
@@ -11,11 +12,13 @@ from dateutil import relativedelta
 
 # Local
 from tasks.models import Task, Claim, Nag, Worker, Work
+from members.models import Member
 
 __author__ = 'adrian'
 
 VC_EMAIL = "Volunteer Coordinator <volunteer@xerocraft.org>"
 XIS_EMAIL = "Xerocraft Internal Systems <xis@xerocraft.org>"
+
 
 class Command(BaseCommand):
 
@@ -55,13 +58,13 @@ class Command(BaseCommand):
         # Process this month's work entries, gathering them by member:
         today = datetime.date.today()
         start_of_month = datetime.datetime(today.year, today.month, 1)
-        for work in Work.objects.filter(work_date__gte=start_of_month):
+        for work in Work.objects.filter(work_date__gte=start_of_month):  # type: Work
             member = work.claim.claiming_member
             if member not in work_lists: work_lists[member] = []
             work_lists[member] += [work]
 
         # Look for work lists with totals that have changed since last report:
-        for member, work_list in work_lists.items():
+        for member, work_list in work_lists.items():  # type: Tuple[Member, List[Work]]
 
             if not member.worker.should_report_work_mtd:
                 logger.info("%s has reportable WMTD but is NOT set for updates.", member)
@@ -70,8 +73,9 @@ class Command(BaseCommand):
             if member.email == "": continue
 
             total_wmtd = datetime.timedelta(0)
-            for work in work_list:
-                total_wmtd += work.work_duration
+            for work in work_list:  # type: Work
+                if work.work_duration is not None:
+                    total_wmtd += work.work_duration
             if total_wmtd != member.worker.last_work_mtd_reported:
                 logger.info("Sent email to %s regarding WMTD = %s", member, total_wmtd)
                 Command.send_report(member, work_list, total_wmtd)
