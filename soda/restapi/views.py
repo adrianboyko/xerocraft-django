c7402a7bc9dae30250df4b7c92cf02384b841e50
adrian <adrianboyko@gmail.com>
2018-06-14 16:08:50 -0700

    Trimmed out stuff left over from cut & paste.

--- soda/restapi/views.py
+++ soda/restapi/views.py
@@ -2,12 +2,9 @@
 # Standard
 
 # Third Party
-from django.contrib.auth.models import User
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Local
-from members.models import Member
 import soda.models as sm
 import soda.restapi.serializers as ts
 import soda.restapi.permissions as tp
@@ -28,5 +25,4 @@ class VendLogViewSet(viewsets.ModelViewSet):
         if user_is_kiosk(self.request):
             return sm.VendLog.objects.all().order_by('id')
         else:
-            # We used to trim down the set for non-kiosk users, but won't, for now.
             return sm.VendLog.objects.all().order_by('id')
