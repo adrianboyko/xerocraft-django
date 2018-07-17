fb0707e3c180659ff390977bf15919407454fdae
adrian <adrianboyko@gmail.com>
2018-07-16 19:00:15 -0700

    Added tabs to layout.

--- elm-clients/dj-ops/src/DjOps.elm
+++ elm-clients/dj-ops/src/DjOps.elm
@@ -1,12 +1,13 @@
 module DjOps exposing (..)
 
 -- Standard
-import Html exposing (Html, div, text, select, option, input)
+import Html exposing (Html, div, text, select, option, input, p, br, span)
 import Html as Html
 import Html.Attributes exposing (style, href, attribute)
 import Html.Events exposing (onClick, on)
 import Http as Http
 import Time exposing (Time, second)
+import Date exposing (Date)
 
 -- Third Party
 import Material
@@ -16,6 +17,9 @@ import Material.Table as Table
 import Material.Options exposing (css)
 import Material.Icon as Icon
 import Material.Layout as Layout
+import Material.Color as Color
+import Material.Footer as Footer
+import DatePicker
 
 -- Local
 import ClockTime as CT
@@ -54,7 +58,10 @@ type alias Model =
   { mdl : Material.Model
   , xis : XisApi.Session Msg
   , time : PointInTime
-  , shows: List XisApi.Show
+  , selectedTab : Int
+  , shows : List XisApi.Show
+  , showDate : Maybe Date
+  , datePicker : DatePicker.DatePicker
   }
 
 
@@ -64,15 +71,19 @@ init flags =
     auth = case flags.csrfToken of
       Just csrf -> DRF.LoggedIn csrf
       Nothing -> DRF.NoAuthorization
+    getShowsCmd = model.xis.listShows ShowList_Result
+    (datePicker, datePickerCmd ) = DatePicker.init
     model =
       { mdl = Material.model
       , xis = XisApi.createSession flags.xisRestFlags auth
       , time = 0
+      , selectedTab = 0
       , shows = []
+      , showDate = Nothing
+      , datePicker = datePicker
       }
-    command = model.xis.listShows ShowList_Result
   in
-    (model, command)
+    (model, Cmd.batch [getShowsCmd, Cmd.map SetDatePicker datePickerCmd])
 
 
 -----------------------------------------------------------------------------
@@ -85,6 +96,8 @@ type
   = Tick Time
   | Mdl (Material.Msg Msg)
   | ShowList_Result (Result Http.Error (DRF.PageOf XisApi.Show))
+  | SelectTab Int
+  | SetDatePicker DatePicker.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -94,6 +107,9 @@ update action model =
 
     Mdl msg_ ->
       Material.update Mdl msg_ model
+
+    SelectTab k ->
+      ( { model | selectedTab = k }, Cmd.none )
 
     Tick newTime ->
       let
@@ -108,45 +124,108 @@ update action model =
     ShowList_Result (Err error) ->
       (model, Cmd.none)
 
+    SetDatePicker msg ->
+      let
+        (newDatePicker, datePickerCmd, dateEvent) =
+          DatePicker.update DatePicker.defaultSettings msg model.datePicker
+        date = case dateEvent of
+          DatePicker.NoChange -> model.showDate
+          DatePicker.Changed newDate -> newDate
+      in
+        ( { model | showDate = date, datePicker = newDatePicker}
+        , Cmd.map SetDatePicker datePickerCmd
+        )
+
 
 -----------------------------------------------------------------------------
 -- VIEW
 -----------------------------------------------------------------------------
 
+tabs model =
+  (
+    [ text "start"
+    , text "tracks"
+    , text "underwriting"
+    , text "membership"
+    ]
+  , [ Color.background (Color.color Color.DeepPurple Color.S400) ]
+  )
 
 view : Model -> Html Msg
 view model =
   Layout.render Mdl model.mdl
   [ Layout.fixedHeader
+  , Layout.onSelectTab SelectTab
   ]
   { header = layout_header model
   , drawer = []
-  , tabs = ([], [])
+  , tabs = tabs model
   , main = [layout_main model]
   }
 
+tagattr x = attribute x x
 
-showOptions : Model -> List (Html Msg)
-showOptions model =
-  List.map
-    (\show -> option [] [text show.data.title])
-    model.shows
+showSelector : Model -> Html Msg
+showSelector model =
+  select [style ["margin-left"=>"20px"], attribute "required" ""] <|
+    ( option
+       [attribute "value" "", tagattr "selected", tagattr "disabled", tagattr "hidden"]
+       [text "Please pick a show..."]
+    )
+
+    ::
+    (
+      List.map
+        (.data >> .title >> text >> List.singleton >> option [])
+        model.shows
+    )
+
+
+showDateSelector : Model -> Html Msg
+showDateSelector model =
+  div [style ["margin-left"=>"20px"]]
+  [ (DatePicker.view
+      model.showDate
+      DatePicker.defaultSettings
+      model.datePicker
+    ) |> Html.map SetDatePicker
+  ]
+
 
 layout_header : Model -> List (Html Msg)
 layout_header model =
   [ Layout.title [css "margin" "20px"]
     [ text "DJ Ops Console"
-    , select [showSelectStyle] (showOptions model)
-    , input [userIdPwInputStyle, attribute "placeholder" "userid", attribute "type" "userid"] []
-    , input [userIdPwInputStyle, attribute "placeholder" "password", attribute "type" "password"] []
     ]
   ]
 
+
 layout_main : Model -> Html Msg
 layout_main model =
-  Table.table []
-  [ Table.tbody []
-    (List.map (tableRow model) (List.range 1 60))
+  case model.selectedTab of
+    0 ->
+      tab_start model
+    1 ->
+      tab_tracks model
+    _ ->
+      p [] [text <| "Tab " ++ toString model.selectedTab ++ " not yet implemented."]
+
+
+tab_start model =
+
+  div [style ["margin"=>"30px", "zoom"=>"1.3"]]
+  [ p [] [text "Welcome to the DJ Ops Console!"]
+  , p [] [text "Choose a show to work on: ", br [] [], showSelector model]
+  , p [] [text "And specify the show date: ", showDateSelector model]
+  ]
+
+
+tab_tracks model =
+  div []
+  [ Table.table [css "margin" "20px"]
+    [ Table.tbody []
+      (List.map (tableRow model) (List.range 1 60))
+    ]
   ]
 
 
@@ -178,7 +257,6 @@ tableRow model r =
 -- SUBSCRIPTIONS
 -----------------------------------------------------------------------------
 
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.batch
@@ -196,11 +274,6 @@ subscriptions model =
 -----------------------------------------------------------------------------
 
 (=>) = (,)
-
-showSelectStyle =
-  style
-  [ "margin-left" => "50px"
-  ]
 
 userIdPwInputStyle =
   style
