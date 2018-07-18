2460b981353ae5864b53fa5521c1cd1f2cd0b69c
adrian <adrianboyko@gmail.com>
2018-07-18 16:40:13 -0700

    Added basics of RFID check-in but not sure we'll keep it. Added dialog for RFID info, but it will be useful for other feedback.

--- elm-clients/dj-ops/src/DjOps.elm
+++ elm-clients/dj-ops/src/DjOps.elm
@@ -8,18 +8,27 @@ import Html.Events exposing (onClick, on)
 import Http as Http
 import Time exposing (Time, second)
 import Date exposing (Date)
+import Regex
+import Char
+import Keyboard
+import Set exposing (Set)
+import Task exposing (Task)
 
 -- Third Party
 import Material
 import Material.Button as Button
 import Material.Textfield as Textfield
 import Material.Table as Table
-import Material.Options exposing (css)
+import Material.Options as Opts exposing (css)
 import Material.Icon as Icon
 import Material.Layout as Layout
 import Material.Color as Color
 import Material.Footer as Footer
 import DatePicker
+import List.Nonempty as NonEmpty exposing (Nonempty)
+import List.Extra as ListX
+import Hex as Hex
+import Dialog as Dialog
 
 -- Local
 import ClockTime as CT
@@ -53,17 +62,27 @@ type alias Flags =
   , xisRestFlags : XisApi.XisRestFlags
   }
 
+type RfidReaderState
+  = Nominal
+  | CheckingAnRfid Int  -- Int counts the number of seconds waited
+  | HitAnHttpErr Http.Error
+  | FoundRfidToBe Bool  -- Bool is True if Rfid was registered, else False.
 
 type alias Model =
   { mdl : Material.Model
   , xis : XisApi.Session Msg
-  , time : PointInTime
+  , currTime : PointInTime
   , selectedTab : Int
   , shows : List XisApi.Show
   , showDate : Maybe Date
   , datePicker : DatePicker.DatePicker
+  , member : Maybe XisApi.Member
+  --- RFID Reader state:
+  , state : RfidReaderState
+  , typed : String
+  , rfidsToCheck : List Int
+  , loggedAsPresent : Set Int
   }
-
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
@@ -76,11 +95,17 @@ init flags =
     model =
       { mdl = Material.model
       , xis = XisApi.createSession flags.xisRestFlags auth
-      , time = 0
+      , currTime = 0
       , selectedTab = 0
       , shows = []
       , showDate = Nothing
       , datePicker = datePicker
+      , member = Nothing
+      --- RFID Reader state:
+      , state = Nominal
+      , typed = ""
+      , rfidsToCheck = []
+      , loggedAsPresent = Set.empty
       }
   in
     (model, Cmd.batch [getShowsCmd, Cmd.map SetDatePicker datePickerCmd])
@@ -98,12 +123,27 @@ type
   | ShowList_Result (Result Http.Error (DRF.PageOf XisApi.Show))
   | SelectTab Int
   | SetDatePicker DatePicker.Msg
+  | KeyDown Keyboard.KeyCode
+  | MemberListResult (Result Http.Error (DRF.PageOf XisApi.Member))
+  | MemberPresentResult (Result Http.Error XisApi.VisitEvent)
+  | AcknowledgeDialog
+
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update action model =
   let xis = model.xis
   in case action of
+
+    AcknowledgeDialog ->
+      ( { model
+        | state = Nominal
+        , typed = ""
+        , rfidsToCheck = []
+        , loggedAsPresent = Set.empty
+        }
+      , Cmd.none
+      )
 
     Mdl msg_ ->
       Material.update Mdl msg_ model
@@ -114,7 +154,7 @@ update action model =
     Tick newTime ->
       let
         seconds = (round newTime) // 1000
-        newModel = { model | time = newTime }
+        newModel = { model | currTime = newTime }
       in
         ( newModel, Cmd.none )
 
@@ -136,6 +176,76 @@ update action model =
         , Cmd.map SetDatePicker datePickerCmd
         )
 
+    KeyDown code ->
+      let
+        typed = case code of
+          16 -> model.typed  -- i.e. ignore this shift code.
+          190 -> ">"  -- i.e. start char resets the typed buffer.
+          c -> model.typed ++ (c |> Char.fromCode |> String.fromChar)
+        finds = Regex.find Regex.All delimitedRfidNum typed
+      in
+        if List.isEmpty finds then
+          -- There aren't any delimited rfids
+          ({model | typed=typed}, Cmd.none)
+        else
+          -- There ARE delimited rfids, so pull them out, process them, and pass a modified s through.
+          let
+            delimitedMatches = List.map .match finds
+            hexMatches = List.map (String.dropLeft 1) delimitedMatches
+            hexToInt = String.toLower >> Hex.fromString
+            resultIntMatches = List.map hexToInt hexMatches
+            intMatches = List.filterMap Result.toMaybe resultIntMatches
+            newRfidsToCheck = ListX.unique (model.rfidsToCheck++intMatches)
+          in
+            checkAnRfid {model | typed=typed, rfidsToCheck=newRfidsToCheck}
+
+    MemberListResult (Ok {results}) ->
+      case results of
+
+        member :: [] ->  -- Exactly ONE match. Good.
+          let
+            cmd2 =
+              if Set.member member.id model.loggedAsPresent then
+                Cmd.none
+              else
+                model.xis.createVisitEvent
+                  { who = model.xis.memberUrl member.id
+                  , when = model.currTime
+                  , eventType = XisApi.VET_Present
+                  , method = XisApi.VEM_FrontDesk
+                  , reason = Nothing
+                  }
+                  MemberPresentResult
+            newModel =
+              { model
+              | state = Nominal
+              , rfidsToCheck = []
+              , typed = ""
+              , loggedAsPresent = Set.insert member.id model.loggedAsPresent
+              }
+          in
+            (newModel, cmd2)
+
+        [] ->  -- ZERO matches. Bad. Should not happen.
+          -- TODO: Log something to indicate a program logic error.
+          checkAnRfid {model | state=FoundRfidToBe False}
+
+        member :: members ->  -- More than one match. Bad. Should not happen.
+          -- TODO: Log something to indicate a program logic error.
+          checkAnRfid {model | state=FoundRfidToBe False}
+
+    MemberPresentResult (Ok _) ->
+      -- Don't need to do anything when this succeeds.
+      (model, Cmd.none)
+
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+    MemberListResult (Err e) ->
+      ({model | state=HitAnHttpErr e}, Cmd.none)
+
+    MemberPresentResult (Err e) ->
+      ({model | state=HitAnHttpErr e}, Cmd.none)
+
 
 -----------------------------------------------------------------------------
 -- VIEW
@@ -154,15 +264,18 @@ tabs model =
 
 view : Model -> Html Msg
 view model =
-  Layout.render Mdl model.mdl
-  [ Layout.fixedHeader
-  , Layout.onSelectTab SelectTab
+  div []
+  [ Layout.render Mdl model.mdl
+    [ Layout.fixedHeader
+    , Layout.onSelectTab SelectTab
+    ]
+    { header = layout_header model
+    , drawer = []
+    , tabs = tabs model
+    , main = [layout_main model]
+    }
+  , Dialog.view <| rfid_dialog_config model
   ]
-  { header = layout_header model
-  , drawer = []
-  , tabs = tabs model
-  , main = [layout_main model]
-  }
 
 
 tagattr x = attribute x x
@@ -219,15 +332,28 @@ tab_start model =
   let
     numTd = td [style ["padding-left"=>"5px", "font-size"=>"24pt", "color"=>"#3f51b5"]]
     instTd = td [style ["padding-left"=>"15px"]]
+    checkTd = td []
     para = p [style ["margin-top"=>"10px"]]
     row = tr []
   in
     div [style ["margin"=>"30px", "zoom"=>"1.3"]]
     [ p [] [text "Welcome to the DJ Ops Console!"]
     , table []
-      [ row [numTd [text "➊ "], instTd [para [text "Use your RFID to log in."]]]
-      , row [numTd [text "➋ "], instTd [para [text "Choose a show to work on: ", br [] [], showSelector model]]]
-      , row [numTd [text "➌ "], instTd [para [text "Specify the show date: ", showDateSelector model]]]
+      [ row
+        [ numTd [text "➊ "]
+        , instTd [para [text "Use your RFID to log in."]]
+        , checkTd [text "✔"]
+        ]
+      , row
+        [ numTd [text "➋ "]
+        , instTd [para [text "Choose a show to work on: ", br [] [], showSelector model]]
+        , checkTd [text "✔"]
+        ]
+      , row
+        [ numTd [text "➌ "]
+        , instTd [para [text "Specify the show date: ", showDateSelector model]]
+        , checkTd [text "✔"]
+        ]
       ]
     ]
 
@@ -274,6 +400,7 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.batch
     [ Time.every second Tick
+    , Keyboard.downs KeyDown
     ]
 
 
@@ -304,3 +431,128 @@ restTdStyle =
   [ css "border-style" "none"
   , css "padding-top" "0"
   ]
+
+
+-----------------------------------------------------------------------------
+-- RFID
+-----------------------------------------------------------------------------
+
+-- Example of RFID data: ">0C00840D"
+-- ">" indicates start of data. It is followed by 8 hex characters.
+-- "0C00840D" is the big endian representation of the ID
+
+delimitedRfidNum = Regex.regex ">[0-9A-F]{8}"
+rfidCharsOnly = Regex.regex "^>[0-9A-F]*$"
+
+
+checkAnRfid : Model -> (Model, Cmd Msg)
+checkAnRfid model =
+  case model.state of
+
+    CheckingAnRfid _ ->
+      -- We only check one at a time, and a check is already in progress, so do nothing.
+      (model, Cmd.none)
+
+    HitAnHttpErr _ ->
+      -- We probably shouldn't even get here. Do nothing, since the error kills any progress.
+      (model, Cmd.none)
+
+    FoundRfidToBe _ ->
+      -- We probably shouldn't even get here. Do nothing.
+      (model, Cmd.none)
+
+    Nominal ->
+      -- We'll check the first one on the list, if it's non-empty.
+      case model.rfidsToCheck of
+
+        rfid :: rfids ->
+          let
+            newModel = {model | rfidsToCheck=rfids, state=CheckingAnRfid 0}
+            memberFilters = [XisApi.RfidNumberEquals rfid]
+            listCmd = model.xis.listMembers memberFilters MemberListResult
+          in
+            (newModel, listCmd)
+
+        [] ->
+          -- There aren't any ids to check. Everything we've tried has failed.
+          ({model | state=FoundRfidToBe False}, Cmd.none)
+
+
+rfid_dialog_config : Model -> Maybe (Dialog.Config Msg)
+rfid_dialog_config model =
+
+  case model.state of
+
+    Nominal ->
+      Nothing
+
+    FoundRfidToBe True ->
+      Nothing
+
+    _ ->
+      Just
+        { closeMessage = Just AcknowledgeDialog
+        , containerClass = Nothing
+        , containerId = Nothing
+        , header = Just (text "🎶 RFID Check-In")
+        , body = Just (rfid_dialog_body model)
+        , footer = Nothing
+        }
+
+
+rfid_dialog_body : Model -> Html Msg
+rfid_dialog_body model =
+
+  case model.state of
+
+    CheckingAnRfid waitCount ->
+      p []
+        [ text "One moment while we check our database."
+        , text (String.repeat waitCount "●")
+        ]
+
+    FoundRfidToBe False ->
+      p []
+        [ text "Couldn't find your RFID in our database."
+        , br [] []
+        , text "Tap the BACK button and try again or"
+        , br [] []
+        , text "speak to a staff member for help."
+        ]
+
+    HitAnHttpErr e ->
+      p []
+        [ text "Tap the BACK button and try again or"
+        , br [] []
+        , text "speak to a staff member for help."
+        ]
+
+    _ -> text ""
+
+
+
+
+-----------------------------------------------------------------------------
+-- TICK (called each second)
+-----------------------------------------------------------------------------
+
+tick : Time -> Model -> (Model, Cmd Msg)
+tick time model =
+  case model.state of
+
+    CheckingAnRfid wc ->
+      ({model | state=CheckingAnRfid (wc+1)}, Cmd.none)
+
+    _ ->
+      (model, Cmd.none)
+
+
+-----------------------------------------------------------------------------
+-- UTILITY
+-----------------------------------------------------------------------------
+
+send : msg -> Cmd msg
+send msg =
+  Task.succeed msg
+
+  |> Task.perform identity
