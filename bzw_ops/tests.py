0b5ff3416de32c702823f60b94b27c84cf8f7f4f
Adrian <adrianboyko@gmail.com>
2018-11-16 19:10:23 -0700

    Reducing test log noise.

--- bzw_ops/tests.py
+++ bzw_ops/tests.py
@@ -28,6 +28,7 @@ ADMIN_FIELDNAME_SINGLES = [
 ]
 
 
+# REVIEW: Is this test useful, anymore? Did Django add similar functionality, somewhere along the way?
 class TestAdminConfig(TestCase):
 
     def test_admin_fieldname_lists(self):
@@ -35,7 +36,7 @@ class TestAdminConfig(TestCase):
         def check_fieldname(fieldname: str, model_class, admin_obj):
             if not isinstance(fieldname, str): return
             fieldname = fieldname.replace("^", "")
-            print("       field: %s" % fieldname)
+            # print("       field: %s" % fieldname)
             if fieldname in dir(model_class):
                 return
             if fieldname in dir(admin_obj):
@@ -45,20 +46,20 @@ class TestAdminConfig(TestCase):
 
         for model_class, admin_obj in admin.site._registry.items():
 
-            print("classes: %s, %s" % (model_class, admin_obj))
+            # print("classes: %s, %s" % (model_class, admin_obj))
 
             # Check lists of field names
             for list_name in ADMIN_FIELDNAME_LISTS:
                 list_of_fieldnames = getattr(admin_obj, list_name)
                 if list_of_fieldnames is None:
                     continue
-                print("    list: %s" % list_name)
+                # print("    list: %s" % list_name)
                 for fieldname in list_of_fieldnames:
                     check_fieldname(fieldname, model_class, admin_obj)
 
             # Check individual field names
             for single_name in ADMIN_FIELDNAME_SINGLES:
-                print("    single: %s" % single_name)
+                # print("    single: %s" % single_name)
                 fieldname = getattr(admin_obj, single_name)
                 check_fieldname(fieldname, model_class, admin_obj)
 
@@ -66,7 +67,7 @@ class TestAdminConfig(TestCase):
             fieldsets = admin_obj.fieldsets
             if fieldsets is not None:
                 for fieldset_name, field_options in fieldsets:
-                    print("    fieldset: %s" % fieldset_name)
+                    # print("    fieldset: %s" % fieldset_name)
                     fields = field_options["fields"]
                     for fieldname in fields:
                         check_fieldname(fieldname, model_class, admin_obj)
