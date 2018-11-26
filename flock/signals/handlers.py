2d727bba322e1f5d0c8b73de7cbe983a4ed3ce1f
Adrian <adrianboyko@gmail.com>
2018-11-26 14:48:27 -0700

    Flock app was never completed and never went into production at XC. This version of the app provides a migration to delete flock tables from production.

--- flock/signals/handlers.py
+++ flock/signals/handlers.py
@@ -8,29 +8,7 @@ from django.db.models.signals import m2m_changed, pre_save, pre_delete, post_sav
 from django.dispatch import receiver
 
 # Local
-from ..models import (
-    Person, PersonInClassTemplate,
-    Resource, ResourceInClassTemplate,
-    TimePattern,
-)
 
 __author__ = 'Adrian'
 
-
-@receiver(post_save, sender=PersonInClassTemplate)
-def post_personinclasstemplate_save(sender, **kwargs):
-    instance = kwargs.get('instance')  # type: PersonInClassTemplate
-    instance.note_personinclasstemplate_change()
-
-
-@receiver(post_save, sender=ResourceInClassTemplate)
-def post_resourceinclasstemplate_save(sender, **kwargs):
-    instance = kwargs.get('instance')  # type: ResourceInClassTemplate
-    instance.note_resourceinclasstemplate_change()
-
-
-@receiver(post_save, sender=TimePattern)
-def post_timepattern_save(sender, **kwargs):
-    instance = kwargs.get('instance')  # type: TimePattern
-    instance.note_timepattern_change()
 
